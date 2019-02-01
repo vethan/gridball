e21f338703b3117f857125d9f3be4acfd2722315
Nathan John <windshaman@googlemail.com>
2019-02-01 14:10:50 +0000

    "Random" turn command

---
+++ GridBall/GridballConsoleGraphix/RandoTurnCommand.cs
@@ -0,0 +1,16 @@
+﻿using GridballCore.TurnCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridballConsoleGraphix
+{
+    class RandoTurnCommand : TurnCommand
+    {
+        protected override int Priority => throw new NotImplementedException();
+
+      //  override 
+    }
+}
