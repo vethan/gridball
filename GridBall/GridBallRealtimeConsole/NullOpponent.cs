1af0f393d96ed3cccab863e4ff7f4c41323c71ff
Nathan John <vethan@unrulyattractions.com>
2019-02-14 12:52:46 +0000

    Missing file

---
+++ GridBall/GridBallRealtimeConsole/NullOpponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GridballCore.TurnCommands;
+
+namespace GridBallRealtimeConsole
+{
+    class NullOpponent : IOpponent
+    {
+        public bool localIsPlayerOne => true;
+
+        public void HandleInput()
+        {
+            
+        }
+
+        public TurnCommand HandleOpponentUpdate(byte frame, TurnCommand myTurnCommand)
+        {
+            return new NullTurnCommand();
+        }
+
+        public void SetupOpponent()
+        {
+        }
+    }
+}
